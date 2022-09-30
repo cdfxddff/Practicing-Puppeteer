bd4b6ac8937494b056caf6c5a1e2c9d26757efc3
yamaguchi <16e4q8yymg@gmail.com>
2022-09-30 23:57:00 +0900

    テキスト取得成功

--- pup.js
+++ pup.js
@@ -4,7 +4,9 @@ const puppeteer=require('puppeteer');
     const browser= await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:3000');
-    await page.screenshot({path:'screenshot2.png'});
+    let sel = await page.$('h1');
+    let value = await (await sel.getProperty('textContent')).jsonValue();
+    console.log(value);
 
     await browser.close();
 })();
