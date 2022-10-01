69b2e737399d7b01b00431f9b70b9096f57a0fcf
yamaguchi <16e4q8yymg@gmail.com>
2022-10-01 13:31:31 +0900

    検索まではできた

--- pup.js
+++ pup.js
@@ -3,8 +3,11 @@ const puppeteer=require('puppeteer');
 (async ()=>{
     const browser= await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-    await page.type('input',"おはようございます");
+    await page.goto('https://dictionary.goo.ne.jp/jn/');
+    //await page.goto('http://localhost:3000');
+    await page.type('#searchtop',"成功");
+    await page.click('input[class="NR-button all_keywordsearchbtn searchbox_form_submit"]');
+    await page.waitForTimeout(1000);
     await page.screenshot({path:'type_result.png'});
     await browser.close();
 })();
